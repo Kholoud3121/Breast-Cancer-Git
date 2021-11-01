8f8d9d9f10acef4052f29cab3a2a90039b7c8d17
Kholoud3121 <75751873+Kholoud3121@users.noreply.github.com>
2021-11-01 06:45:34 +0300

    Add files via upload

---
+++ API (1).py
@@ -0,0 +1,31 @@
+import requests
+
+print("HotSPot")
+print("HotSpot is a website that has been created to help researchers and physicians to know if the  genetic variations"
+      "are reported to be associated with cancer")
+
+
+def account():
+    print("Create an account")
+    name = input("Enter your name:")
+    email = input("Enter your e-mail:")
+    print("Hello " + name +
+          " a conformation e-mail will be sent to your e-mail " + email)
+
+
+def variation():
+    return input("Enter the the gene variation using the transcript HGVS expressions, e.g. NM_000314.4:c.395G>T:")
+
+
+def search(Variation):
+    url = "https://rest.variantvalidator.org/VariantValidator/tools/gene2transcripts/"
+    response = requests.get(url+Variation)
+    js = response.json()
+
+    print("The gene is a "+js['transcripts'][0]['description'])
+
+
+account()
+Variation = variation()
+print("Searching for" + Variation)
+search(Variation)
